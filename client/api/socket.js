99e46cb6a00c7bb342afb575e5ebab898ce2a604
Mohammed Ismail <100583896+Mr-Som3a@users.noreply.github.com>
2025-09-03 23:42:59 +0400

    Update socket.js

--- client/api/socket.js
+++ client/api/socket.js
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 
 // socket.js
-export const socket = io(import.meta.env.MODE === "development"?`${import.meta.env.VITE_SERVER_URL}`:'/');
+export const socket = io(import.meta.env.VITE_SERVER_URL);
