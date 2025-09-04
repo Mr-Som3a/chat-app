a4455ab2e1c02bb4e75a15903e9f1e41e6ead557
Mr.Som3a <mrsom3a8@gmail.com>
2025-09-04 10:06:17 +0400

    update 2.0

--- server/config/socket.js
+++ server/config/socket.js
@@ -9,7 +9,7 @@ const userSocketMap = {}; //{userId,socketId}
 
 const io = new Server(server, {
     cors: {
-      origin: ["http://localhost:8080"],
+        origin: ["http://localhost:8080","https://chat-app-client-side-s4sj.onrender.com"],
     },
   });
 
