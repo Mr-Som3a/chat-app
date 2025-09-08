5673931e87d8133b57d47a4be09da56653bb8baa
Mr.Som3a <mrsom3a8@gmail.com>
2025-09-08 15:14:17 +0400

    fix login bug

--- client/api/httpServices.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const axiosInstance = axios.create({
-    baseURL:import.meta.env.MODE === "development"? `${import.meta.env.VITE_SERVER_URL}/api`:"/api",
-    withCredentials:true,
-    headers: { "auth-x": `Bearer ${localStorage.getItem("token")}` }
-})
